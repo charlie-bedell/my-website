0414bc2d3c72d1a07ecb501c872eefc61b95fd6a
Charles Bedell <charlesbedell5@gmail.com>
2024-03-10 14:16:57 -0400

    styling for content component

--- app/ui/RepoContent/RepoContent.tsx
+++ app/ui/RepoContent/RepoContent.tsx
@@ -7,8 +7,8 @@ interface RepoContentProps {
 const RepoContent: React.FC<RepoContentProps> = ({ type, title, content}) => {
   return (
 		<div>
-			<article>
-				<h1>{title}</h1>
+			<article className="text-black flex flex-col gap-1 m-2 p-4 pt-2 bg-slate-500">
+				<h1 className="font-bold">{title}</h1>
 				<p>{content}</p>
 			</article>
 		</div>
