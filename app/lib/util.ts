1810cb4c1511b4962ac3dd398bed1990cac5e718
Charles Bedell <charlesbedell5@gmail.com>
2024-03-21 16:08:31 -0400

    add sql and config packages, minor changes

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,7 +1,7 @@
 export const getGitHubUserFromEnv = () => {
 	const GH_USER = process.env.GITHUB_USER;
 	if (GH_USER === undefined) {
-		throw new Error("a github user is not defined in the .env under NEXT_PUBLIC_GITHUB_USER");
+		throw new Error("a github user is not defined in the .env under GITHUB_USER");
 	} else {
 		return GH_USER;
 	}
