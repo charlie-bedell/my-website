027ce0a503eb18bf0e9c215ac98ea5ed2966ca48
Charles Bedell <charlesbedell5@gmail.com>
2024-04-11 17:04:06 -0400

    add testimage back in

---
+++ app/lib/data.ts
@@ -0,0 +1,3 @@
+export const testImage = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.lovepik.com%2Ffree-png%2F20211125%2Flovepik-square-cat-yellow-cartoon-angry-expression-pack-png-image_401129516_wh1200.png&f=1&nofb=1&ipt=cdf15c3a9033e687ef79040236f78a7db6ffc70d91ec8df9ea1ebf8c9fd9c514&ipo=images";
+
+export * as data from './data';
