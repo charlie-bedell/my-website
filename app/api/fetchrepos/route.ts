fc2bc44554d53f09cb2e882f30babbd4b4e335be
Charlie Bedell <30674761+charlie-bedell@users.noreply.github.com>
2024-04-12 15:44:37 -0400

    Revert "Feature/server fetch repo data"

--- app/api/fetchrepos/route.ts
+++
@@ -1,7 +0,0 @@
-import { retrieveRepos } from "@/app/lib/services/githubApi"
-import { repoData } from "@/app/lib/definitions";
-
-export async function GET() {
-  const data: repoData = await retrieveRepos();
-  return Response.json({ data })
-}
